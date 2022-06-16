b9d0605d08268a89476284ab10a28fecd6ea867e
Zach Parks <zach@alliware.com>
2022-06-15 23:04:23 -0500

    Removed default export for archipelago.js, incremented version number as changes break current API, and configured tsc to not output javascript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import { ArchipelagoClient } from "./ArchipelagoClient";
-
 export * from "@enums";
 export * from "@managers";
 export * from "@packets";
 export * from "@structs";
 
 export * from "./ArchipelagoClient";
-
-export default ArchipelagoClient;
