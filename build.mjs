3091b09102c2b5c483a2cb0433beb473bf5981f9
Zach Parks <zach@alliware.com>
2024-11-04 07:34:45 -0600

    Use bun for building src (same as v2)

---
+++ build.mjs
@@ -0,0 +1,14 @@
+import dts from "bun-plugin-dts";
+
+await Bun.build({
+    entrypoints: ["./src/index.ts"],
+    outdir: "./dist",
+    plugins: [dts()],
+});
+
+// Standalone.
+await Bun.build({
+    entrypoints: ["./src/index.ts"],
+    outdir: "./dist",
+    naming: "[dir]/archipelago.min.[ext]",
+});
