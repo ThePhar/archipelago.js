6eb181246c3b0b1fc33fce163db6663ec527c434
Zach Parks <zach@alliware.com>
2024-11-05 20:56:05 -0600

    Overhaul message event handling
    * Rename some message events and add events for each PrintJSON message type the server can send.
    * Remove `index` from message event params (unnecessary as it will always be `MessageManager.count - 1` when fired).
    * All message events return `MessageNode[]` instead of `PrintJSONPacket` as their last argument.
    * Rename `MessageManager.messages` to `MessageManager.log` to be more accurate.
    * MessageManager.log returns a shallow copy of all chat messages instead of direct array to prevent accidental modification.
    * Instead of exposing PrintJSON packets, instead exposes abstracted MessageNode objects in `MessageManager.log`.
    * The "any message" event now fires after all specific message events have fired.

--- src/events/MessageEvents.ts
+++ src/events/MessageEvents.ts
@@ -9,127 +9,131 @@ import { Player } from "../classes/Player.ts";
 export type MessageEvents = {
     /**
      * Fires when any kind of message is received.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    message: [message: string, nodes: MessageNode[]]
+    message: [text: string, nodes: MessageNode[]]
 
     /**
      * Fires when another player is sent an item (except for cheated items).
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param item The item being sent.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    itemSent: [message: string, item: Item, nodes: MessageNode[]]
+    itemSent: [text: string, item: Item, nodes: MessageNode[]]
 
     /**
      * Fires when another player is sent a cheated item.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param item The item being sent.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    itemCheated: [message: string, item: Item, nodes: MessageNode[]]
+    itemCheated: [text: string, item: Item, nodes: MessageNode[]]
 
     /**
      * Fires when a hint-style message is received.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param item The item being hinted.
      * @param found If the item was found.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      * @remarks This event is for hint messages received. To track more information on when hints are updated, utilize
      * one of the hint-type events on {@link ItemsManager} instead.
      */
-    itemHinted: [message: string, item: Item, found: boolean, nodes: MessageNode[]]
+    itemHinted: [text: string, item: Item, found: boolean, nodes: MessageNode[]]
 
     /**
      * Fires when a client connects to the room session.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param player The player being connected as.
      * @param tags The tags of the joining client.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    connected: [message: string, player: Player, tags: string[], nodes: MessageNode[]]
+    connected: [text: string, player: Player, tags: string[], nodes: MessageNode[]]
 
     /**
      * Fires when a client disconnects from the room session.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param player The player being disconnected from.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    disconnected: [message: string, player: Player, nodes: MessageNode[]]
+    disconnected: [text: string, player: Player, nodes: MessageNode[]]
 
     /**
      * Fires when a player chat message is received.
-     * @param message The plaintext message content.
+     * @param message The plaintext message content without the sender name prefix.
      * @param player The metadata of the player who sent this message.
+     * @param nodes An array of message nodes in this message with additional context for each textual component.
+     * @remarks `nodes` will contain the sender name prefix from the server, but `message` will not.
      */
-    chat: [message: string, player: Player]
+    chat: [message: string, player: Player, nodes: MessageNode[]]
 
     /**
      * Fires when a server chat message is received.
-     * @param message The plaintext message content.
+     * @param message The plaintext message content without the sender name prefix.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
+     * @remarks `nodes` will contain the sender name prefix from the server, but `message` will not.
      */
     serverChat: [message: string, nodes: MessageNode[]]
 
     /**
      * Fires when tutorial-like information is received, such as on first connection explaining `!help`.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    tutorial: [message: string, nodes: MessageNode[]]
+    tutorial: [text: string, nodes: MessageNode[]]
 
     /**
      * Fires when a client updates their tags.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param player The player this client is connected to.
      * @param tags The new tags for this client.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    tagsUpdated: [message: string, player: Player, tags: string[], nodes: MessageNode[]]
+    tagsUpdated: [text: string, player: Player, tags: string[], nodes: MessageNode[]]
 
     /**
      * Fires on the result of running a user command, such as `!status`.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    userCommand: [message: string, nodes: MessageNode[]]
+    userCommand: [text: string, nodes: MessageNode[]]
 
     /**
      * Fires on the result of running an admin command via `!admin`.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    adminCommand: [message: string, nodes: MessageNode[]]
+    adminCommand: [text: string, nodes: MessageNode[]]
 
     /**
      * Fires when a connected player has met their goal condition.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param player The player that reached their goal.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    goaled: [message: string, player: Player, nodes: MessageNode[]]
+    goaled: [text: string, player: Player, nodes: MessageNode[]]
 
     /**
      * Fires when a player has released their remaining items to the multi-world.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param player The player that released.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    released: [message: string, player: Player, nodes: MessageNode[]]
+    released: [text: string, player: Player, nodes: MessageNode[]]
 
     /**
      * Fires when a player has collected their remaining items from the multi-world.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param player The player that collected.
      * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    collected: [message: string, player: Player, nodes: MessageNode[]]
+    collected: [text: string, player: Player, nodes: MessageNode[]]
 
     /**
      * Fires when a countdown message is received.
-     * @param message The plaintext message content.
+     * @param text The plaintext message content.
      * @param value The current countdown value.
+     * @param nodes An array of message nodes in this message with additional context for each textual component.
      */
-    countdown: [message: string, value: number]
+    countdown: [text: string, value: number, nodes: MessageNode[]]
 };
